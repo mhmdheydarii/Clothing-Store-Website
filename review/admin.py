79d741fa9c2ca9b06bc9946ecaedd3024ec3268f
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-09-26 03:31:07 -0700

    Create : Admin.py for review app

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import ReviewModel
 # Register your models here.
+
+@admin.register(ReviewModel)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["user", "product", "rate", "created_date", "status"]
+    list_filter = ["status"]
+    
